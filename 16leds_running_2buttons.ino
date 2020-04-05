f39f2b14a396fcfe09d45194b27ae8ec7d313cc4
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2020-04-05 15:44:49 +0300

    Update 16leds_running_2buttons.ino

--- 16leds_running_2buttons.ino
+++ 16leds_running_2buttons.ino
@@ -20,7 +20,7 @@ void setup() {
 
   
   // use a for loop to initialize each pin as an output:
-  for (int thisPin = 1; thisPin <= 16; thisPin++) {
+  for (int thisPin = 2; thisPin <= 17; thisPin++) {
     pinMode(thisPin, OUTPUT);
   }
  
@@ -43,7 +43,7 @@ delay(50);
 void inainte()  // forward
 {
    // loop from the lowest pin to the highest:
-  for (int thisPin = 1; thisPin <= 16; thisPin++) {
+  for (int thisPin = 2; thisPin <= 17; thisPin++) {
     // turn the pin on:
     digitalWrite(thisPin, HIGH);
     delay(timer);
@@ -55,7 +55,7 @@ void inainte()  // forward
 void inapoi()  // backward
 {
   // loop from the highest pin to the lowest:
-  for (int thisPin = 16; thisPin >= 1; thisPin--) {
+  for (int thisPin = 17; thisPin >= 2; thisPin--) {
     // turn the pin on:
     digitalWrite(thisPin, HIGH);
     delay(timer);
