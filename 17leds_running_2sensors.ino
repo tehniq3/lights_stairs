160fa1840be6f9a2c998574df4bd7c7c9f570b8c
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2020-12-05 11:10:59 +0200

    Add files via upload

---
+++ 17leds_running_2sensors.ino
@@ -0,0 +1,102 @@
+/*
+  original created in 2006 by David A. Mellis
+  modified 30 Aug 2011   by Tom Igoe
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/ForLoop
+  adapted for maximum 17 leds and 2 buttons by Nicu FLORICA (niq_ro), 03.11.2018
+  adapted for maximum 17 leds and 2 sensors by Nicu FLORICA (niq_ro), 05.12.2020
+  https://nicuflorica.blogspot.com/
+*/
+
+#define senzor1 A4 // first sensor
+#define senzor2 A5 // other sensor
+
+int timer = 50;           // The higher the number, the slower the timing.
+int intarziere = 3000;     // delay for lit on at 100%77
+int n = 17;  // number of leds (max 17)
+
+void setup() {
+  pinMode(senzor1, INPUT);
+  pinMode(senzor2, INPUT);
+//  digitalWrite(senzor1, HIGH);
+//  digitalWrite(senzor2, HIGH); 
+  
+  // use a for loop to initialize each pin as an output:
+  for (int thisPin = 1; thisPin <= n; thisPin++) {
+    pinMode(thisPin, OUTPUT);
+  }
+}
+
+void loop() {
+if (digitalRead(senzor1) == HIGH)
+{
+  inainte();
+  delay(intarziere);
+  inainte2();
+}
+if (digitalRead(senzor2) == HIGH)
+{
+  inapoi();
+  delay(intarziere);
+  inapoi2();
+}
+byte j = 100;
+
+ for (int thisPin = n; thisPin >= 1; thisPin--) {
+    // turn the pin on:
+    digitalWrite(thisPin, HIGH);
+    delayMicroseconds(1);
+    // turn the pin off:
+    digitalWrite(thisPin, LOW);
+    delayMicroseconds(50);
+  } 
+
+
+}  // end main loop
+
+void inainte()  // forward
+{
+   // loop from the lowest pin to the highest:
+  for (int thisPin = 1; thisPin <= n; thisPin++) {
+    // turn the pin on:
+    digitalWrite(thisPin, HIGH);
+    delay(timer);
+    // turn the pin off:
+   // digitalWrite(thisPin, LOW);
+  }
+}
+void inainte2()  // forward
+{
+   // loop from the lowest pin to the highest:
+  for (int thisPin = 1; thisPin <= n; thisPin++) {
+    // turn the pin on:
+    digitalWrite(thisPin, LOW);
+    delay(timer);
+    // turn the pin off:
+   // digitalWrite(thisPin, LOW);
+  }
+}
+
+void inapoi()  // backward
+{
+  // loop from the highest pin to the lowest:
+  for (int thisPin = n; thisPin >= 1; thisPin--) {
+    // turn the pin on:
+    digitalWrite(thisPin, HIGH);
+    delay(timer);
+    // turn the pin off:
+    // digitalWrite(thisPin, LOW);
+  } 
+}
+
+void inapoi2()  // backward
+{
+  // loop from the highest pin to the lowest:
+  for (int thisPin = n; thisPin >= 1; thisPin--) {
+    // turn the pin on:
+    digitalWrite(thisPin, LOW);
+    delay(timer);
+    // turn the pin off:
+    //digitalWrite(thisPin, LOW);
+  } 
+}
